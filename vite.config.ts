ba51005085823b04689de390da4110abe0badbc0
Roy Malka <roy.malka@roy.malka-MBP>
2024-04-06 10:28:25 +0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -13,8 +12,7 @@ export default defineConfig({
       "/api": {
         target: "https://www.via-explorer-api.roymalka.dev",
         changeOrigin: true,
-        secure: false, // Set to true if your target endpoint is HTTPS
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: true,
       },
     },
   },
