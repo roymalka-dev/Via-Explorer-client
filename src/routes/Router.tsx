7355e033e1aaaae34c665c69ce5bc9b95fbe6ece
Roy Malka <roy.malka@roy.malka-MBP>
2024-04-09 22:29:42 +0300

    router fix

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,50 +4,51 @@ import {
   Route,
 } from "react-router-dom";
 import { Suspense } from "react";
-import { RouteObject } from "@/types/routes.types";
-import { routes } from "@/configs/routes.config";
-import AuthorityGuard from "./AuthorityGuard";
+import { RouteObject } from "@/types/routes.types"; // Type definition for route objects
+import { routes } from "@/configs/routes.config"; // Importing route configurations
+import AuthorityGuard from "./AuthorityGuard"; // Importing the AuthorityGuard component for protected routes
 
-// Auth
-
+/**
+ * This configuration sets up the application's routing using `react-router-dom`.
+ * It leverages the `createBrowserRouter` and `createRoutesFromElements` to dynamically
+ * create routes based on a predefined configuration (`routes`).
+ *
+ * Each route is wrapped with lazy loading using `Suspense` and an optional `AuthorityGuard`
+ * for route protection based on user roles or permissions.
+ */
 export const router = createBrowserRouter(
   createRoutesFromElements(
+    // Mapping over the `routes` configuration to create route elements
     routes?.map((route: RouteObject) => {
       return (
-        /* Protected Route Gate */
+        // Wrapping each route with a protection layer (if defined)
         <Route
           key={route.key + "protect"}
           path={route.path}
-          element={
-            <Suspense fallback={<route.loader />}>
-              <route.protect />
-            </Suspense>
-          }
+          element={<route.protect />}
         >
-          {/* Layout */}
+          {/* Main route element with optional layout and lazy loading */}
           <Route
             key={route.key}
             path={route.path}
             element={
               <Suspense fallback={<route.loader />}>
+                {/* Redirect if specified, else render the main route component */}
                 {route.redirect && route.redirect}
                 <route.element />
               </Suspense>
             }
           >
+            {/* Nested routes for child components, if any */}
             {route.children?.map((child: RouteObject) => {
               return (
-                /* Authority Guard */
+                // Each child route is wrapped with an `AuthorityGuard` for access control
                 <Route
                   key={child.key + "auth"}
                   path={child.path}
-                  element={
-                    <Suspense fallback={<child.loader />}>
-                      <AuthorityGuard pageAuth={child.authority || ""} />
-                    </Suspense>
-                  }
+                  element={<AuthorityGuard pageAuth={child.authority || ""} />}
                 >
-                  {/* Page */}
+                  {/* Child route element with lazy loading */}
                   <Route
                     key={child.key}
                     path={child.path}
